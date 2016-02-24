f3d12b0b6d1790b02b57fcb5319220c621981a26
mns4e <michans4e@gmail.com>
2016-02-24 11:08:54 +0100

    eclipse add choose server from docker

--- eclipse/orientdb-tutorial-distributed-database/src/main/Manager.java
+++ eclipse/orientdb-tutorial-distributed-database/src/main/Manager.java
@@ -1,14 +1,20 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 import com.orientechnologies.orient.client.remote.OServerAdmin;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import com.orientechnologies.orient.object.db.OObjectDatabaseTx;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.regex.Pattern;
+
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
 
 
 
@@ -16,16 +22,17 @@ public class Manager {
 
 		private OObjectDatabaseTx db;
 		private BufferedReader br;
+		private Map<String,String> dockerIPs;
 	
 		void startLoop(){
 	
 			System.out.println("welcome, what do you want to do?");
+			System.out.println("'c' | connect");
+			System.out.println("'d' | disconnect");
 			System.out.println("'a' | add");
 			System.out.println("'r' | remove");
 			System.out.println("'s' | show");
 			System.out.println("'q' | quit");
-			
-			// todo connect/dis
 			
 			String sInput = "";
 			br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,43 +44,97 @@ public class Manager {
 			
 			if(sInput == null) sInput = "";
 			
-			// connect to database
-			connect();
 			
 			while (!sInput.equals("quit")&&!sInput.equals("q")) 
 			{
 				switch (sInput) 
 				{
-					case "add":    case "a": add();     break;
-					case "remove": case "r": remove();  break;
-					case "show":   case "s": show();    break;
-					default: System.out.println("???"); break;
+					case "connect":		case "c": connect();    break;
+					case "disconnect":	case "d": disconnect();	break;
+					case "add":			case "a": add();     	break;
+					case "remove":		case "r": remove();  	break;
+					case "show":		case "s": show();    	break;
+					default: System.out.println("???"); 		break;
 				}
-				System.out.println("____________________________________________________________________\nand now (a|r|s|q)?");
+				System.out.println("____________________________________________________________________\nand now (c|d|a|r|s|q)?");
 				try{
 		        	sInput = br.readLine();
 		        } catch(Exception e){}
 			}
 			
 			// disconnect database
-			close();
+			disconnect();
 			
 			System.out.println("bye");
 		}
 		
 		void connect()
-		{			
+		{		
+			disconnect();
 			
+			dockerIPs = new HashMap<String,String>();
 			
+			if (System.getProperty("os.name").startsWith("Windows")) {
+		        // includes: Windows 2000,  Windows 95, Windows 98, Windows NT, Windows Vista, Windows XP
+		    } else {
+		        // everything else
+				try {
+					// get available docker names an IPs
+					String cmdSH = 
+							"#!/bin/bash \n"+
+							"array=( $(docker ps | awk '{if(NR>1) print $NF}') ) \n"+
+							"for i in ${array[@]} \n"+
+							"do \n"+
+							"        echo ${i}'::'$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${i}) \n"+
+							"done\n";
+							
+					String[] cmd = new String[]{"/bin/bash","-c",cmdSH};
+					Process p = Runtime.getRuntime().exec(cmd);
+					p.waitFor();
+				    BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+				    String line = "";			
+				    while ((line = reader.readLine())!= null) {    	
+				    	dockerIPs.put(line.split("::")[0],line.split("::")[1]);
+				    }
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		    	
+		    } 
 			
-			// wählen welcher docker container
-			//  docker network inspect bridge 
-			//  docker inspect -f '{{ .NetworkSettings.Name }}' 6950151d643a
-			//  docker ps -q
+
+			System.out.println("Please enter the Database-Server-IP address:  (e.g. \"172.17.0.3\")");
+
+			if(!dockerIPs.isEmpty())
+			{
+				System.out.println("Available servers from docker:");
+				for (Map.Entry<String, String> entry : dockerIPs.entrySet())
+				{
+					System.out.println(entry.getValue()+ ":" + entry.getKey());
+				}
+			}
+			
+			String sIP = "";
+			br = new BufferedReader(new InputStreamReader(System.in));
+		    Pattern p = Pattern.compile("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+
+			
+			while (true) {
+				try {
+					sIP = br.readLine();
+				} catch (Exception e) {
+				}
+				if(sIP != null && !sIP.isEmpty() && p.matcher(sIP).find()) break;
+				System.out.println("???");
+			}
+			
+
 			
 			try 
 			{
-				String sIP="172.17.0.3";
 				
 				// CREATE A SERVER ADMIN CLIENT AGAINST A REMOTE SERVER TO CHECK IF DB EXISTS				
 				OServerAdmin oSAdmin = new OServerAdmin("remote:"+sIP+"/test").connect("root","root");
@@ -99,10 +160,10 @@ public class Manager {
 			}
 		}
 		
-		void close()
+		void disconnect()
 		{
 			// disconnect database
-			if(db != null)
+			if(db != null && !db.isClosed())
 			{
 				db.close();
 				System.out.println("disconnect");
@@ -112,6 +173,7 @@ public class Manager {
 
 		void add()
 		{
+			if(db==null || db.isClosed()) connect();
 			// generate random customer and add it to the database
 			String sR = String.valueOf(new Random().nextInt(900)+100);
 			Customer c = new Customer("s"+sR,"n"+sR,"str"+sR,"city"+sR);
@@ -121,6 +183,7 @@ public class Manager {
 		
 		void remove()
 		{
+			if(db==null || db.isClosed()) connect();
 			// get all customers from database
 			List<Customer> lstC = db.query(new OSQLSynchQuery<Customer>("select * from Customer"));
 			
@@ -139,6 +202,17 @@ public class Manager {
 		
 		void show()
 		{
+			if(db==null || db.isClosed()) connect();
+			
+//			String sCluster;
+//			for (String iterable_element : iterable) {
+//				
+//			}
+//			{
+//				
+//				
+//			}
+			
 			// get all customers from database and print it
 			List<Customer> lstC = db.query(new OSQLSynchQuery<Customer>("select * from Customer"));
 			
